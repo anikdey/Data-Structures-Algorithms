53c0024df0e4572e1d1a43f4e6cab730373f132b
Anik Dey <info.anikdey003@gmail.com>
2021-05-30 20:22:08 +0600

    leetcode string easy problems solved

---
+++ LeetCode/src/string/DetermineColorOfChessboardSquare.java
@@ -0,0 +1,34 @@
+package string;
+
+public class DetermineColorOfChessboardSquare {
+
+    public static void main(String[] args) {
+        System.out.println(squareIsWhite("a1"));
+        System.out.println(squareIsWhite("e5"));
+        System.out.println(squareIsWhite("h8"));
+    }
+
+    public static boolean squareIsWhite(String coordinates) {
+
+        int firstCoordinate = coordinates.charAt(0);
+        int secondCoordinate = coordinates.charAt(1);
+//        boolean isWhite = true;
+//        if(firstCoordinate % 2 == 1 && secondCoordinate % 2 == 1) {
+//            isWhite = false;
+//        } else if(firstCoordinate % 2 == 1 && secondCoordinate % 2 == 0) {
+//            isWhite = true;
+//        } else if(firstCoordinate % 2 == 0 && secondCoordinate % 2 == 1) {
+//            isWhite = true;
+//        } else {
+//            isWhite = false;
+//        }
+        boolean isWhite = false;
+       if(firstCoordinate % 2 == 1 && secondCoordinate % 2 == 0) {
+            isWhite = true;
+        } else if(firstCoordinate % 2 == 0 && secondCoordinate % 2 == 1) {
+            isWhite = true;
+        }
+        return isWhite;
+    }
+
+}
