23912229f4b1ebe222bf557f11c55792155be6bb
Anik Dey <info.anikdey003@gmail.com>
2021-06-27 03:05:53 +0600

    leetcode array easy problem solved

--- LeetCode/src/array/easy/CreateTargetArrayInGivenOrder.java
+++ LeetCode/src/array/easy/CreateTargetArrayInGivenOrder.java
@@ -1,23 +1,30 @@
-package array;
+package array.easy;
 
 import java.util.Arrays;
 
 public class CreateTargetArrayInGivenOrder {
 
     public static void main(String[] args) {
+        //int[] nums = {0,1,2,3,4};
+        //int[] index = {0,1,2,2,1};
+
         int[] nums = {0,1,2,3,4};
-        int[] index = {0,1,2,2,1};
+        int[] index = {0,2,2,1,1};
+
+
         System.out.println(Arrays.toString(createTargetArray(nums, index)));
     }
 
     public static int[] createTargetArray(int[] nums, int[] index) {
         int[] targetArray = new int[nums.length];
         for(int i=0; i<index.length; i++) {
+
             int num = nums[i];
             int ind = index[i];
+
             if(ind<i) {
-                for(int x=ind+1; x<nums.length-1; x++) {
-                    targetArray[x] = targetArray[x-1];
+                for(int x=i-1; x>=ind; x--) {
+                    targetArray[x+1] = targetArray[x];
                 }
             }
             targetArray[ind] = num;
