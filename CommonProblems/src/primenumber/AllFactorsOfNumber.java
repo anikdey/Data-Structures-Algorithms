20cef493769aae269c8b0ba15a4816f09af9dcb3
Anik Dey <info.anikdey003@gmail.com>
2021-06-29 04:13:21 +0600

    finding all factors of a number

---
+++ CommonProblems/src/primenumber/AllFactorsOfNumber.java
@@ -0,0 +1,40 @@
+package primenumber;
+
+public class AllFactorsOfNumber {
+
+    public static void main(String[] args) {
+        //allFactorsNaiveApproach(36);
+        allFactorsOptimized(49);
+    }
+
+    public static void allFactorsNaiveApproach(int number) {
+        System.out.println(1);
+        int half = number/2;
+        for(int i=2; i<=half; i++) {
+            if(number%i == 0) {
+                System.out.println(i);
+            }
+        }
+        System.out.println(number);
+//        for(int i=1; i<=number; i++) {
+//            if(number%i == 0) {
+//                System.out.println(i);
+//            }
+//        }
+    }
+
+
+    public static void allFactorsOptimized(int number) {
+        int sqrt = (int) Math.sqrt(number);
+        for(int i=1; i<=sqrt; i++) {
+            if(number % i == 0) {
+                System.out.println(i);
+                if(i != sqrt) {
+                    System.out.println(number/i);
+                }
+            }
+
+        }
+    }
+    
+}
---
+++ CommonProblems/src/primenumber/PrimeNumber.java
@@ -0,0 +1,12 @@
+package primenumber;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+}
