b9de5aa25fc0f7e01144884a0b42f0022b70500d
Anik Dey <info.anikdey003@gmail.com>
2021-06-30 08:49:11 +0600

    project euler 10001st prime solved

---
+++ ProjectEuler/src/Problem_7_10001stPrime.java
@@ -0,0 +1,29 @@
+public class Problem_7_10001stPrime {
+
+    public static void main(String[] args) {
+        System.out.println(findNthPrime(10001));
+    }
+
+    public static long findNthPrime(int n) {
+        long lastPrime = 1;
+        long number = 2;
+        while (n != 0) {
+            if (isPrime(number)) {
+                lastPrime = number;
+                n--;
+            }
+            number++;
+        }
+        return lastPrime;
+    }
+
+    public static boolean isPrime(long number) {
+        double sqrt = Math.sqrt(number);
+        for(int i=2; i<=sqrt; i++) {
+            if(number%i == 0)
+                return false;
+        }
+        return true;
+    }
+
+}
