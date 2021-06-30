94afb749b480bcd163da11c2962138408ce698d2
Anik Dey <info.anikdey003@gmail.com>
2021-06-30 08:47:35 +0600

    find nth prime naive approach

---
+++ CommonProblems/src/primenumber/FindNthPrime.java
@@ -0,0 +1,31 @@
+package primenumber;
+
+public class FindNthPrime {
+
+    public static void main(String[] args) {
+        System.out.println(findNthPrime(10001));
+    }
+
+    public static long findNthPrime(int n) {
+        long lastPrime = 1;
+        long number = 2;
+        while (n != 0) {
+            if (isPrime(number)) {
+                lastPrime = number;
+                n--;
+            }
+            number++;
+        }
+        return lastPrime;
+    }
+
+    public static boolean isPrime(long number) {
+        double sqrt = Math.sqrt(number);
+        for(int i=2; i<=sqrt; i++) {
+            if(number%i == 0)
+                return false;
+        }
+        return true;
+    }
+
+}
