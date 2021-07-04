f8f9923904aa53ca3a8e7321a24a5987b9320c27
Anik Dey <info.anikdey003@gmail.com>
2021-07-05 03:18:16 +0600

    outermost parenthesis solved

---
+++ LeetCode/src/string/RemoveOutermostParentheses.java
@@ -0,0 +1,44 @@
+package string;
+
+import java.util.Stack;
+
+public class RemoveOutermostParentheses {
+
+    public static void main(String[] args) {
+        System.out.println(removeOuterParentheses("(()())(())"));
+        System.out.println(removeOuterParentheses("()()()"));
+        System.out.println(removeOuterParentheses("(()())(())(()(()))"));
+    }
+
+    public static String removeOuterParentheses(String s) {
+        if(s.isEmpty())
+            return "";
+        StringBuilder stringBuilder = new StringBuilder();
+
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        int stackSize = 0;
+        for(int i=0; i<chars.length; i++) {
+            char currentChar = chars[i];
+            if(stackSize==0) {
+                stack.push(currentChar);
+                stackSize++;
+            } else {
+                char top = stack.peek();
+                if(top == '(' && currentChar == ')') {
+                    stack.pop();
+                    stackSize--;
+                    if(stackSize>0) {
+                        stringBuilder.append(currentChar);
+                    }
+                } else {
+                    stack.push(currentChar);
+                    stackSize++;
+                    stringBuilder.append(currentChar);
+                }
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+}
