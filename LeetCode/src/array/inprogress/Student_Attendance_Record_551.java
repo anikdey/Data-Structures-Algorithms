87e7ed486c373d5dea41672f98442e6b1fda6817
Anik Dey <info.anikdey003@gmail.com>
2021-10-21 19:47:19 +0600

    easy problem solved

---
+++ LeetCode/src/array/inprogress/Student_Attendance_Record_551.java
@@ -0,0 +1,38 @@
+package array.inprogress;
+
+public class Student_Attendance_Record_551 {
+
+    public static void main(String[] args) {
+
+        System.out.println(checkRecord("LALL"));
+
+    }
+
+    public static boolean checkRecord(String s) {
+
+        char[] chars = s.toCharArray();
+
+        int absentCount = 0;
+        int lateCount = 0;
+
+
+        for(char ch : chars) {
+            if(ch == 'A') {
+                absentCount++;
+                if(absentCount>1)
+                    return false;
+            } else if(ch == 'L') {
+                lateCount++;
+                if(lateCount>2)
+                    return false;
+            } else {
+                lateCount = 0;
+            }
+        }
+
+
+
+        return true;
+    }
+
+}
